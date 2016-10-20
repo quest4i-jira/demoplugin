20c5287fce0d2e7b9d220d1959868241cf7dfb89
사명구(Sa Myeonggu) <quest4i@me.com>
2016-10-20 11:49:49 +0900

    chapter3 ReadOnlyUserCF 모듈 추가

---
+++ src/test/java/ut/com/jtricks/jira/customfields/ReadOnlyUserCFTest.java
@@ -0,0 +1,34 @@
+package ut.com.jtricks.jira.customfields;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import com.jtricks.jira.customfields.ReadOnlyUserCF;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @since 3.5
+ */
+public class ReadOnlyUserCFTest {
+
+    @Before
+    public void setup() {
+
+    }
+
+    @After
+    public void tearDown() {
+
+    }
+
+    @Test(expected=Exception.class)
+    public void testSomething() throws Exception {
+
+        //ReadOnlyUserCF testClass = new ReadOnlyUserCF();
+
+        throw new Exception("ReadOnlyUserCF has no tests!");
+
+    }
+
+}
