9ceb72144454c2dfdb70b6abc4b1a27ef4598f4c
사명구(Sa Myeonggu) <quest4i@me.com>
2016-10-27 15:49:06 +0900

    fix if error and add method for updating custom field

--- src/main/java/com/jtricks/jira/customfields/ReadOnlyUserCF.java
+++ src/main/java/com/jtricks/jira/customfields/ReadOnlyUserCF.java
@@ -1,6 +1,9 @@
 package com.jtricks.jira.customfields;
 
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.CustomFieldManager;
 import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.ModifiedValue;
 import com.atlassian.jira.issue.changehistory.ChangeHistory;
 import com.atlassian.jira.issue.changehistory.ChangeHistoryManager;
 import com.atlassian.jira.issue.customfields.impl.GenericTextCFType;
@@ -9,6 +12,8 @@ import com.atlassian.jira.issue.customfields.persistence.CustomFieldValuePersist
 import com.atlassian.jira.issue.fields.CustomField;
 import com.atlassian.jira.issue.fields.TextFieldCharacterLengthValidator;
 import com.atlassian.jira.issue.fields.layout.field.FieldLayoutItem;
+import com.atlassian.jira.issue.fields.layout.field.FieldLayoutManager;
+import com.atlassian.jira.issue.util.DefaultIssueChangeHolder;
 import com.atlassian.jira.security.JiraAuthenticationContext;
 import com.atlassian.plugin.spring.scanner.annotation.component.Scanned;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
@@ -16,6 +21,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+
+import static com.sun.corba.se.spi.activation.IIOP_CLEAR_TEXT.value;
+import static java.util.Optional.ofNullable;
 
 
 @Scanned
@@ -26,6 +34,7 @@ public class ReadOnlyUserCF extends GenericTextCFType {
     private final JiraAuthenticationContext jiraAuthenticationContext;
 
     private final ChangeHistoryManager changeHistoryManager;
+
 
     protected ReadOnlyUserCF(
             @ComponentImport
@@ -41,7 +50,7 @@ public class ReadOnlyUserCF extends GenericTextCFType {
 
         super(customFieldValuePersister, genericConfigManager,
                 textFieldCharacterLengthValidator, jiraAuthenticationContext);
-        
+
         this.jiraAuthenticationContext = jiraAuthenticationContext;
         this.changeHistoryManager = changeHistoryManager;
     }
@@ -53,20 +62,36 @@ public class ReadOnlyUserCF extends GenericTextCFType {
                                                      final FieldLayoutItem fieldLayoutItem) {
 
         final Map<String, Object> params = super.getVelocityParameters(issue, field, fieldLayoutItem);
-        String currentUser = jiraAuthenticationContext.getLoggedInUser().getName();
-        params.put("currentUser", currentUser);
+        String value = jiraAuthenticationContext.getLoggedInUser().getName();
+        params.put("value", value);
 
         if (issue.isCreated()) {
-            Optional<List<ChangeHistory>> optHistory = Optional.ofNullable(
+            Optional<List<ChangeHistory>> optHistory = ofNullable(
                     changeHistoryManager.getChangeHistories(issue));
-            if (optHistory.isPresent()) {
+            if (optHistory.isPresent() && (! optHistory.get().isEmpty())) {
                 Collections.reverse(optHistory.get());
-                String lastModifier = optHistory.get().get(0).getAuthorObject().getName();
-                log.warn("01 lastModifier : " + lastModifier);
-                params.put("lastModifier", lastModifier);
+                value = optHistory.get().get(0).getAuthorObject().getName();
+                log.warn("01 lastModifier : " + value);
+                params.put("value", value);
             }
         }
 
         return params;
+    }
+
+
+
+
+    public void setValue(Issue issue, String new_value) {
+
+        CustomFieldManager customFieldManager = ComponentAccessor.getCustomFieldManager();
+        CustomField customField = customFieldManager.getCustomFieldObject("customfield_10102");
+        String old_value = (String)issue.getCustomFieldValue(customField);
+        ModifiedValue modifiedValue = new ModifiedValue(old_value, new_value);
+
+        FieldLayoutManager fieldLayoutManager = ComponentAccessor.getFieldLayoutManager();
+        FieldLayoutItem fieldLayoutItem = fieldLayoutManager.getFieldLayout(issue)
+                .getFieldLayoutItem(customField);
+        customField.updateValue(fieldLayoutItem, issue, modifiedValue, new DefaultIssueChangeHolder());
     }
 }
